87cbd2723a9e4e1e98e7b42608171d5952fb744f
root <root@ip-172-31-12-89.ap-southeast-2.compute.internal>
2025-01-27 13:45:17 +0000

    test-changed

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test
     public static void main(String[] args){
         System.out.println("Hello World!");
 	System.out.println("This is from linux");
+	System.out.println("Iam from server");
     }
 }

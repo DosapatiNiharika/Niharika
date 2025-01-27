c6e13ca1eb9d84e7d0adcae50cebc6120cdc824d
DosapatiNiharika <155795902+DosapatiNiharika@users.noreply.github.com>
2025-01-27 16:21:12 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,5 @@ public class test
     public static void main(String[] args){
         System.out.println("Hello World!");
 	System.out.println("This is from linux");
-	System.out.println("for th dev conflict");
     }
 }
